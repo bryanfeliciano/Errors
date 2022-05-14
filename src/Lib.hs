8cbeef4415eb64bfa04b49c0b77745ee4a7eb7eb
bryan <bryanfeliciano892@gmail.com>
2022-05-14 18:38:25 -0500

    head and partial functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,9 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+myTake :: Int -> [a] -> [a]
+myTake 0 _ = []
+myTake n xs = head xs : myTake (n - 1) (tail xs)
+
+myTakePM :: Int -> [a] -> [a]
+myTakePM 0 _ = []
+myTakePM n (x:xs) = x : myTakePM (n - 1) xs
