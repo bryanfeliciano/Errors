03c7c9780297d7dbea6c7e67836f516151b01665
bryan <bryanfeliciano892@gmail.com>
2022-05-14 18:50:51 -0500

    intro to either type for error handling

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,3 +7,20 @@ myTake n xs = head xs : myTake (n - 1) (tail xs)
 myTakePM :: Int -> [a] -> [a]
 myTakePM 0 _ = []
 myTakePM n (x:xs) = x : myTakePM (n - 1) xs
+
+-- multiple ways to handle errors --
+
+myHead :: [a] -> a 
+myHead [] = error "empty list"
+myHead (x:_) = x
+
+maybeHead :: [a] -> Maybe a
+maybeHead [] = Nothing
+maybeHead (x:_) = Just x
+
+myTakeSafer :: Int -> Maybe [a] -> Maybe [a]
+myTakeSafer 0 _ = Just []
+myTakeSafer n (Just xs) = (:) <$> maybeHead xs
+                              <*> myTakeSafer (n - 1) (Just (tail xs))
+
+-- Intro To either type --
